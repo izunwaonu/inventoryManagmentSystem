c211d1f7af757ee29303c73e43cbef393b7af65e
swaat <izunwaonu2@gmail.com>
2024-11-27 15:14:23 +0100

    updated

--- app/(back-office)/dashboard/home/overview/page.jsx
+++ app/(back-office)/dashboard/home/overview/page.jsx
@@ -1,26 +1,24 @@
-import DashboardBanner from '@/components/dashboard/DashboardBanner'
-import SalesOverview from '@/components/dashboard/SalesOverview'
-import React from 'react'
-import CurrentStock from '@/components/dashboard/CurrentStock'
-import { getData } from '@/lib/getData'
+import DashboardBanner from '@/components/dashboard/DashboardBanner';
+import SalesOverview from '@/components/dashboard/SalesOverview';
+import CurrentStock from '@/components/dashboard/CurrentStock';
+import { getData } from '@/lib/getData';
 
 export default async function Dashboard() {
+  const items = await getData('items');
+  const warehouses = await getData('warehouse');
 
-  const items = await getData("items")
-  const warehouses = await getData("warehouse")
- 
   return (
     <div>
-      {/* <DashboardBanner/> */}
-      <SalesOverview/>
+      {/* <DashboardBanner /> */}
+      <SalesOverview />
       <CurrentStock title="Available Stock, Item Stock" items={items} />
-      {
-        warehouses.map((warehouse, i) =>{
-          return (
-            <CurrentStock key={i} title={`Available Stock Items ${warehouse.title}`} items={warehouse.items}/>
-          )
-        })
-      }
+      {warehouses.map((warehouse, i) => (
+        <CurrentStock
+          key={i}
+          title={`Available Stock Items ${warehouse.title}`}
+          items={warehouse.items}
+        />
+      ))}
     </div>
-  )
+  );
 }
