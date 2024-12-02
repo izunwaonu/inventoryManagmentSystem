2daa74ef6b3e0f42dff3b931c2360a8532d4141b
swaat <izunwaonu2@gmail.com>
2024-12-02 05:55:50 +0100

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,43 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+    
+//         images: {
+//           remotePatterns: [
+//             {
+//               protocol: 'https',
+//               hostname: 'res.cloudinary.com',
+             
+//             },
+//             {
+//               protocol: 'https',
+//               hostname: 'utfs.io',
+             
+//             },
+//           ],
+//         },
+        
+      
+// };
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-        images: {
-          remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'res.cloudinary.com',
-             
-            },
-            {
-              protocol: 'https',
-              hostname: 'utfs.io',
-             
-            },
-          ],
-        },
-      
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+    ],
+  },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL, // Expose the DATABASE_URL from environment variables
+  },
 };
 
 export default nextConfig;
+
